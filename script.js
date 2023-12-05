6eea024b8c034fe71511a5987dcd87ee46c238e9
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-12-05 23:29:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const conatiner=document.getElementById("timer");
+const date =new Date();
+let cdate=getDate();
+conatiner.innerText=cdate;
+
+function getDate(date) {
+	let Y= date.getFullYear();
+	let M=date.getMonth();
+	let D=date.getDate()
+
+	return D+'/'+M+'/'+Y
+}
